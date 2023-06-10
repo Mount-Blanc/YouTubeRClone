8a6f93f0314275d67bd32985f7779b8547dcc59b
Mount-Blanc <mountblanc100@gmail.com>
2023-06-10 11:45:14 -0700

    added header categories section

--- youtubeclone/src/App.jsx
+++ youtubeclone/src/App.jsx
@@ -7,16 +7,32 @@ function App() {
     <>
      <header>
       <div className="header-container">
-      <div>
-        <button>button</button>
+      <div className="nav-left">
+        <button>nav</button>
+        <span>logo</span>
       </div>
-      <div>
+      <div className="nav-middle">
         <form>
           <input placeholder="Search"/>
-          <button></button>
+          <button>S</button>
         </form>
+      </div >
+      <div className="nav-right">right</div>
       </div>
-      <div>right</div>
+      <div className="header-categories">
+        <button>All</button>
+        <button>Mixes</button>
+        <button>Gaming</button>
+        <button>Music</button>
+        <button>Psychology</button>
+        <button>Podcasts</button>
+        <button>Soundtracks</button>
+        <button>Japanese Music</button>
+        <button>Live</button>
+        <button>Manga</button>
+        <button>Jazz</button>
+        <button>Computer Science</button>
+
       </div>
      </header>
     </>
